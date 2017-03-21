94b6091b5f617bf2bf07cbb5ab482fb14e6f4064
Administrator <Administrator@PC-20160724FCXS>
2017-03-21 10:48:15 +0800

    第一次提交

---
+++ 放大镜进阶/js/design.js
@@ -0,0 +1,61 @@
+
+function Subject(content){
+	var content = content;//发布的内容
+	var subcribers = new List();//订阅者们
+	//订阅
+	this.subcribe = function(client){
+		subcribers.add(client);
+	}
+	//退订
+	this.unsubcribe = function(client){
+		subcribers.remove(client);
+	}
+	//发布动作
+	this.publish = function(){
+		for(var i in subcribers){
+			subcribers[i].notice(content);//通知每个订阅者
+		}
+	}
+}
+
+function Email(){
+	//......
+	this.notice = function(msg){
+		alert("观察者接到的消息内容："+msg);
+	}
+}
+
+/*function List(){
+	//...自己封装的集合,
+	this.add = function(){...}
+	this.remove = function(){...}
+}*/
+
+
+var s1 = new Subject("暴走大事件");
+var s2 = new Subject("人人都是产品经理");
+var s3 = new Subject("罗辑思维");
+
+//每个Email代表一个用户邮箱
+var email01 = new Email();
+var email02 = new Email();
+//你不仅可以用Email订阅，还能用手机订阅
+var phone01 = new Phone();
+var phone02 = new Phone();
+
+s1.subcribe(email01);//邮箱1和2订阅了《大事件》
+s1.subcribe(email02);//
+
+s2.subcribe(email01);//邮箱1订阅了《产品经理》
+
+s3.subcribe(email02);//邮箱2订阅了《罗辑思维》
+s3.subcribe(phone01);//手机1订阅了《逻辑思维》
+
+s1.publish();
+s2.publish();
+s3.publish();
+
+
+
+
+
---
+++ 放大镜进阶/js/my.jquery.hover.js
@@ -0,0 +1,71 @@
+$.fn.hoverDir = function(time){
+	$(this).time = time || 1000;
+	$.Hover.init($(this));	
+	$(this).live("mouseenter mouseleave", function(event){
+		var x = (event.clientX+$(window).scrollLeft() - this.offsetLeft - (this.offsetWidth/2)) * (this.offsetWidth > this.offsetHeight ? this.offsetHeight/this.offsetWidth : 1);
+		var y = (event.clientY+$(window).scrollTop() - this.offsetTop - (this.offsetHeight/2)) * (this.offsetHeight > this.offsetWidth ? this.offsetWidth/this.offsetHeight : 1);
+		var direction = Math.round((((Math.atan2(y, x) * (180 / Math.PI))) / 90) + 5) % 4; 
+		if(event.type == "mouseenter"){
+			$.Hover.over(direction, $(this));
+		} else {
+			$.Hover.out(direction, $(this));
+		}
+	});
+}
+$.Hover = {
+	init : function($this){
+		var cover = $("<div></div>").css({
+			position:"absolute",
+			width:$this.outerWidth(),
+			height:$this.outerHeight(),
+			background:"rgba(1,1,1,0.6)",
+			left:0,
+			top:"-100%"
+		}).addClass("cover");
+		$this.append(cover);
+	},
+	over : function(dir, $target){
+		var $cover = $target.find(".cover"); 
+		$cover.stop();
+		switch(dir) {
+			case 0 :
+				$cover.css("left","0").css("top","-100%").animate({left:0,top:0},$target.time); break;
+			case 1 :
+				$cover.css("left","100%").css("top","0").animate({left:0,top:0},$target.time); break;
+			case 2 :
+				$cover.css("left","0").css("top","100%").animate({left:0,top:0},$target.time); break;
+			case 3 :
+				$cover.css("left","-100%").css("top","0").animate({left:0,top:0},$target.time); break;
+		}
+	},
+	out : function(dir, $target){
+		var $cover = $target.find(".cover"); 
+		$cover.stop();
+		switch(dir) {
+			case 0 :
+				$cover.animate({left:0,top:"-100%"},$target.time); break;
+			case 1 :
+				$cover.animate({left:"100%",top:0},$target.time); break;
+			case 2 :
+				$cover.animate({left:0,top:"100%"},$target.time); break;
+			case 3 :
+				$cover.animate({left:"-100%",top:0},$target.time); break;
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
